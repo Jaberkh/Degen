d5b33a98e9890cc051624633bbdd8006783e9191
Jaber <khodadadi.jaber@live.com>
2024-12-04 22:12:05 +0330

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -261,7 +261,7 @@ app.frame("/", async (c) => {
   const composeCastUrl = `https://warpcast.com/~/compose?text=${encodeURIComponent(
     "Check Your Degen State\nFrame By @jeyloo\n"
   )}&embeds[]=${encodeURIComponent(
-    `https://7dd9-16-24-71-66.ngrok-free.app/?${urlParams.toString()}`
+    `https://degen-state.onrender.com/?${urlParams.toString()}`
   )}`;
 
   console.log("Generated Share URL:", composeCastUrl);
